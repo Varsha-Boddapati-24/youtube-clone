e073613ed15ea4a9d53ebb27e441ec587f8b3b66
varsha_boddapati_24 <varshaboddapati24@gmail.com>
2025-07-12 23:07:18 +0530

    change in secure setting

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -60,8 +60,8 @@ export const loginUser = async (req, res) => {
             .status(200)
             .cookie("token", token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: 'lax',
+                secure: true,
+                sameSite: 'none',
                 maxAge: 24 * 60 * 60 * 1000,
             })
             .json({
